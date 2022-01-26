ad2908084b324a8ca153742686cfd8ffdf497baa
d0nal <d0naldhuynhdang@gmail.com>
2022-01-25 22:33:57 -0600

    Changed main to use vec3 instead of doubles

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 #include <fstream>
 
@@ -20,17 +23,12 @@ int main() {
 
 	for (int j = image_height-1; j >= 0; --j)
 	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto r = double(i) / (image_width-1);
-			auto g = double(j) / (image_width-1);
-			auto b = 0.25;
-
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			helloFile << ir << ' ' << ig << ' ' << ib << '\n';
+			color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+//			color pixel_color(0,0,0);
+			write_color(helloFile, pixel_color);
 		}
 	}
 
