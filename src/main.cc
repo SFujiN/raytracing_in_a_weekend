848bebc2d045117e91c6e10a0948d4909bd1e7e4
d0nal <d0naldhuynhdang@gmail.com>
2022-01-26 00:16:04 -0600

    width is now 3840p

--- src/main.cc
+++ src/main.cc
@@ -37,7 +37,7 @@ int main() {
 	// Image
 	
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 1920;
+	const int image_width = 3840;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
 	// Camera
