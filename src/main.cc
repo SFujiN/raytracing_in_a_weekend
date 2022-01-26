24de6d3e38ed5d62bee9a89504892c8ec57ac70c
d0nal <d0naldhuynhdang@gmail.com>
2022-01-26 17:19:59 -0600

    smaller pixel sampling

--- src/main.cc
+++ src/main.cc
@@ -14,17 +14,25 @@
   \param world hittable&, hittable object representing objects in the world.
   \return The color of the pixel to be drawn in the scene.
  */
-color ray_color(const ray& r, const hittable& world)
+color ray_color(const ray& r, const hittable& world, int depth)
 {
 	hit_record rec;
+	
+	// If we've exceeded the ray bounce limit, no more light is gathered.
+	if (depth <= 0)
+	{
+		return color(0,0,0);
+	}
+
 	if (world.hit(r, 0, infinity, rec))
 	{
-		return 0.5 * (rec.normal + color(1,1,1));
+		point3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
 	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5*(unit_direction.y() + 1.0);
-//	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
-	return (1.0-t)*color(0.5, 0.7, 1.0) + t*color(0, 0, 0);
+	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+//	return (1.0-t)*color(0.5, 0.7, 1.0) + t*color(0, 0, 0);
 }
 
 int main() {
@@ -38,15 +46,17 @@ int main() {
 	// Image
 	
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 960;
+	const int image_width = 3840;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
+	const int samples_per_pixel = 10;
+	const int max_depth = 50;
 
 	// World
 	
 	hittable_list world;
 	world.add(std::make_shared<sphere>(point3(0,0,-1), 0.5));
 	world.add(std::make_shared<sphere>(point3(0,-100.4,-1), 100));
+//	world.add(std::make_shared<sphere>(point3(-1,0,-1), 0.5));
 
 	// Camera
 	
@@ -67,7 +77,7 @@ int main() {
 				auto u = (i + random_double()) / (image_width-1);
 				auto v = (j + random_double()) / (image_height-1);
 				ray r = cam.get_ray(u, v);
-				pixel_color += ray_color(r, world);
+				pixel_color += ray_color(r, world, max_depth);
 			}
 //			color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);	// color gradient
 //			color pixel_color(0,0,0);							// black
