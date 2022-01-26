c0baaeb6bd529f6e8671b96edd1da33ae1d5644e
d0nal <d0naldhuynhdang@gmail.com>
2022-01-26 14:01:30 -0600

    now it's 960x540 cause hd is a lot

--- src/main.cc
+++ src/main.cc
@@ -36,7 +36,7 @@ int main() {
 	// Image
 	
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 1920;
+	const int image_width = 960;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
 	// World
