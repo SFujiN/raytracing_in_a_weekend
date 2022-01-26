bd16afcfc2a67e8c81992a64fc4f5f4a79307db9
d0nal <d0naldhuynhdang@gmail.com>
2022-01-26 14:31:10 -0600

    added more utility with random number generator

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -1,6 +1,7 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
+#include <cstdlib>
 #include <cmath>
 #include <limits>
 #include <memory>
@@ -15,6 +16,31 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
 	return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+	// Returns a random real in [0,1).
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+	// Returns a random real in [min,max).
+	return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max)
+{
+	if (x < min)
+	{
+		return min;
+	}
+	if (x > max)
+	{
+		return max;
+	}
+	return x;
 }
 
 // Common Headers
