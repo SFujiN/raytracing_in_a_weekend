fbbcc57bdcb81de1dbe4d7cb069f09ad43bca195
d0nal <d0naldhuynhdang@gmail.com>
2022-01-26 02:37:21 -0600

    some documentation for vec3.h

--- src/vec3.h
+++ src/vec3.h
@@ -4,39 +4,84 @@
 #include <cmath>
 #include <iostream>
 
+//!	vec3 struct.
+/*!
+	A struct to represent 3D vectors.
+*/
 typedef struct vec3
 {
+	//!	An array of doubles.
+	/*!	An array of doubles to store the x, y, and z coordinates of a vec3. */
 	double e[3];
 
+	//!	vec3 constructor.
+	/*!	default constructor for vec3. */
 	vec3() : e{0,0,0} {}
+	//!	vec3 constructor.
+	/*!
+		\param e0 double representing x position.
+		\param e1 double representing y position.
+		\param e2 double representing z position.
+	*/
 	vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
+	//!	function to return the x position
+	/*!
+	 	\return e[0] which represents the x position.
+	*/
 	double x() const 
 	{
 		return e[0];
 	}
+	//!	function to return the y position
+	/*!
+	 	\return e[1] which represents the y position.
+	*/
 	double y() const
 	{
 		return e[1];
 	}
+	//!	function to return the z position
+	/*!
+	 	\return e[2] which represents the z position.
+	*/
 	double z() const
 	{
 		return e[2];
 	}
 
+	//!	overloaded unary negation operator for vec3.
+	/*!
+		\return vec3 negation of current vec3
+	*/
 	vec3 operator-() const
 	{
 		return vec3(-e[0], -e[1], -e[2]);
 	}
+	//!	overloaded subscript operator for vec3.
+	/*!
+	 	\param i int for index of element in e.
+	 	\return the element of e in index i.
+	*/
 	double operator[](int i) const
 	{
 		return e[i];
 	}
+	//!	overloaded subscript operator for vec3.
+	/*!
+	 	\param i int for index of element in e.
+		\return the address of element e in index i.
+	*/
 	double& operator[](int i)
 	{
 		return e[i];
 	}
 
+	//!	overloaded increment assignment operator for vec3.
+	/*!
+	 	\param v the vector being added then assigned to current vector.
+		\return pointer to current vector.
+	*/ 
 	vec3& operator+=(const vec3 &v)
 	{
 		e[0] += v.e[0];
@@ -45,6 +90,11 @@ typedef struct vec3
 		return *this;
 	}
 
+	//!	overloaded multiplication assignment operator for vec3.
+	/*!
+	 	\param t double to scale elements of e to.
+		\return pointer to current vector.
+	*/ 
 	vec3& operator*=(const double t)
 	{
 		e[0] *= t;
@@ -54,16 +104,29 @@ typedef struct vec3
 		
 	}
 
+	//!	overloaded divide assignment operator for vec3.
+	/*!
+	 	\param t double to divide the elements of e by.
+		\return pointer to current vector.
+	*/
 	vec3& operator/=(const double t)
 	{
 		return *this *= 1/t;
 	}
 
+	//!	length function to return the length of current vec3.
+	/*!
+	 	\param double representing the length of the current vector.
+	*/
 	double length() const
 	{
 		return std::sqrt(length_squared());
 	}
 
+	//!	length_squared function to find the square of each element in e added together.
+	/*!
+	 	\return the sum of the elements of e squared.
+	*/
 	double length_squared() const
 	{
 		return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
@@ -71,6 +134,7 @@ typedef struct vec3
 
 } point3, color;
 
+// TODO: the rest of the documentation
 // vec3 Utility Functions
 
 inline std::ostream& operator<<(std::ostream &out, const vec3 &v)
