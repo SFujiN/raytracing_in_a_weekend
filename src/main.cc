04188e8ef16d443f33c04703737f124812dc729c
d0nal <d0naldhuynhdang@gmail.com>
2022-01-26 20:34:07 -0600

    Changed the scene a litte and now resolution is 1280x720(HD) for time purposes

--- src/main.cc
+++ src/main.cc
@@ -24,15 +24,15 @@ color ray_color(const ray& r, const hittable& world, int depth)
 		return color(0,0,0);
 	}
 
-	if (world.hit(r, 0, infinity, rec))
+	if (world.hit(r, 0.001, infinity, rec))
 	{
 		point3 target = rec.p + rec.normal + random_in_unit_sphere();
 		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
 	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5*(unit_direction.y() + 1.0);
-	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
-//	return (1.0-t)*color(0.5, 0.7, 1.0) + t*color(0, 0, 0);
+//	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+	return (1.0-t)*color(0.5, 0.7, 1.0) + t*color(0, 0, 0);
 }
 
 int main() {
@@ -46,17 +46,17 @@ int main() {
 	// Image
 	
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 3840;
+	const int image_width = 1280;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 10;
+	const int samples_per_pixel = 100;
 	const int max_depth = 50;
 
 	// World
 	
 	hittable_list world;
-	world.add(std::make_shared<sphere>(point3(0,0,-1), 0.5));
-	world.add(std::make_shared<sphere>(point3(0,-100.4,-1), 100));
-//	world.add(std::make_shared<sphere>(point3(-1,0,-1), 0.5));
+	world.add(std::make_shared<sphere>(point3(0.5,0,-2), 0.5));
+	world.add(std::make_shared<sphere>(point3(-0.5,0,-2), 0.5));
+	world.add(std::make_shared<sphere>(point3(0,-100.4,-2), 100));
 
 	// Camera
 	
