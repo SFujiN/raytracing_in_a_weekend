e7eecbb66eebe9ae7c9634a707217beaec957904
d0nal <d0naldhuynhdang@gmail.com>
2022-01-27 19:56:03 -0600

    Issue resolved, metal needed constructor, also adding fuzziness

--- src/material.h
+++ src/material.h
@@ -10,7 +10,7 @@ typedef struct material
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
 } material;
 
-typedef struct lambertian : public material
+typedef struct lambertian : material
 {
 	color albedo;
 
@@ -36,11 +36,14 @@ typedef struct lambertian : public material
 typedef struct metal : material
 {
 	color albedo;
+	double fuzz;
+
+	metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override
 	{
 		vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
 		attenuation = albedo;
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
