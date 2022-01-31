a188b7aca4aa8e3cfc283da874987224ff25aa98
d0nal <d0naldhuynhdang@gmail.com>
2022-01-30 18:22:02 -0600

    move renderstep to a function

--- src/main.cc
+++ src/main.cc
@@ -121,6 +121,28 @@ hittable_list my_scene()
 	return world;
 }
 
+void render(std::ostream& out, const camera& cam, const hittable_list& world, int image_width, int image_height, int samples_per_pixel, int max_depth)
+{
+	out << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height-1; j >= 0; --j)
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i)
+		{
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s)
+			{
+				auto u = (i + random_double()) / (image_width-1);
+				auto v = (j + random_double()) / (image_height-1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world, max_depth);
+			}
+			write_color(out, pixel_color, samples_per_pixel);
+		}
+	}
+}
+
 int main() {
 	// File
 	std::ofstream helloFile;
@@ -153,7 +175,9 @@ int main() {
 	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 	
 	// Render
-	
+
+	render(helloFile, cam, world, image_width, image_height, samples_per_pixel, max_depth);
+	/*	
 	helloFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = image_height-1; j >= 0; --j)
@@ -174,6 +198,7 @@ int main() {
 			write_color(helloFile, pixel_color, samples_per_pixel);
 		}
 	}
+	*/
 
 	helloFile.close();
 
