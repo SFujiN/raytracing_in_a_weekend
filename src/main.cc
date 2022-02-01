ca8129df9b28810ca4aa97dd2e7e7e74e5c8bad5
d0nal <d0naldhuynhdang@gmail.com>
2022-01-31 18:50:04 -0600

    Change scene to cover scene

--- src/main.cc
+++ src/main.cc
@@ -162,7 +162,7 @@ int main() {
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 3840; // nHD: 640, qHD: 960, HD: 1280, Full HD: 1920, QHD: 2560, 4K UHD: 3840
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 500;
+	const int samples_per_pixel = 100;
 	const int max_depth = 50;
 
 	// World
